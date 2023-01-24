c752779a184885dca3e718b483650c9b370f9b59
Divyansh Agarwal <divyanshagarwal188@gmail.com>
2023-01-24 16:27:17 +0530

    __repr__()__

--- cool_concepts/dunders/dunders.py
+++ cool_concepts/dunders/dunders.py
@@ -22,9 +22,12 @@ class Vector:
 
     def __add__(self, vectortypeobj):
         return Vector(self.x+vectortypeobj.x, self.y+vectortypeobj.y)
+    
+    def __repr__(self): # reperesents how object is printed
+        return f"x = {self.x} | y = {self.y}"
         
 v1 = Vector(1,2)
 v2 = Vector(3,4)
 v3 = v1+v2 # gives error if + is not overloaded with the help of __add__()
 print(f"v3 = {v3.x, v3.y}")
-        
+print(v3)
