f4e84d07d540dbd28475d544a153b80e44ea5051
Divyansh Agarwal <divyanshagarwal188@gmail.com>
2023-01-24 16:17:57 +0530

    operator overloading

--- cool_concepts/dunders/dunders.py
+++ cool_concepts/dunders/dunders.py
@@ -1,13 +1,30 @@
-
 # double underscore methods
+
+
 class Student:
     def __init__(self, age, name): # constructor
         self.age = age
         self.name = name
         
-    def __del__(self):
-        print("Object deleted")
+    def __del__(self): # deconstructor
+        print("Object deleted") 
         
 s1= Student(22, "divyansh")
 print(s1.age)
 print(s1.name)
+
+
+# operator overloading
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, vectortypeobj):
+        return Vector(self.x+vectortypeobj.x, self.y+vectortypeobj.y)
+        
+v1 = Vector(1,2)
+v2 = Vector(3,4)
+v3 = v1+v2 # gives error if + is not overloaded with the help of __add__()
+print(f"v3 = {v3.x, v3.y}")
+        
