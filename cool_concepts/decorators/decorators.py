81d33f11ed7399c907fd4d8d8db75a5c5cb36777
Divyansh Agarwal <divyanshagarwal188@gmail.com>
2023-01-25 20:34:16 +0530

    pass params

--- cool_concepts/decorators/decorators.py
+++ cool_concepts/decorators/decorators.py
@@ -16,5 +16,21 @@ def hello():
 def hello_world():
     print("Hello World!")
     
-hello_world()
+#hello_world()
 
+# the upper decorator function does not work when we have arguments in our functions that we want to decorate
+# solution: add *args, **kwargs
+
+def decorater(function):
+    
+    def wrapper(*args, **kwargs):
+        print("decorating your function!")
+        function(*args, **kwargs)
+        
+    return wrapper
+
+@decorater
+def hello_user(user):
+    print(f"Hello {user}!")
+
+hello_user("divyansh")
