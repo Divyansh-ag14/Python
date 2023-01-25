a7a5fe73a7f817c334323451323968d2be7214da
Divyansh Agarwal <divyanshagarwal188@gmail.com>
2023-01-25 21:22:04 +0530

    time function using decorator

---
+++ cool_concepts/decorators/timing.py
@@ -0,0 +1,26 @@
+# use decorator to show execution time of function
+
+import time
+
+def timer(func):
+    
+    def wrapper(*args, **kwargs):
+        print("timing your function!")
+        s = time.time()
+        val = func(*args, **kwargs)
+        e = time.time()
+        print(f"function execution time {e-s} seconds!")
+        return val
+        
+    return wrapper
+
+@timer
+def func():
+    result=0
+    for i in range(1,100000000):
+        result+=i
+        
+    return result
+
+func() 
+#print(func())
