d666ce1b1d80c50029cae47cd827492bd31c6c9d
Divyansh Agarwal <divyanshagarwal188@gmail.com>
2023-01-26 22:37:42 +0530

    size of object

--- cool_concepts/generators/generators.py
+++ cool_concepts/generators/generators.py
@@ -1,5 +1,7 @@
 # generators are used for on demand sourcing
 
+import sys
+
 def square_generator(n):
     for x in range(1,n):
         yield x*x
@@ -7,6 +9,13 @@ def square_generator(n):
 values = square_generator(8)
 for x in values:
     print(x)
+
+# changing the values of params does not change the size of the object
+obj1 = square_generator(8)
+obj2 = square_generator(16)
+print(sys.getsizeof(obj1))
+print(sys.getsizeof(obj2))
+
 
 # working of yield and next
 def gen():
