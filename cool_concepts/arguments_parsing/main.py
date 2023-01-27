5420a2b8d209304d2c058197f256dd6e74a8d0bf
Divyansh Agarwal <divyanshagarwal188@gmail.com>
2023-01-27 20:18:40 +0530

    write to a file using console

--- cool_concepts/arguments_parsing/main.py
+++ cool_concepts/arguments_parsing/main.py
@@ -3,3 +3,13 @@ import sys
 # when we run "python main.py xyyz" on console
 print(sys.argv[0]) # prints "main.py"
 print(sys.argv[1]) #prints "xyz"
+
+#print the entire argument vector
+print(sys.argv)
+
+# writing to a new file using console
+file = sys.argv[1]
+msg = sys.argv[2:]
+
+with open(file, 'w+') as f:
+    f.write(" ".join(msg))
