1fe374c6d882a017d4617b81596c12ed3c657bbd
Divyansh Agarwal <divyanshagarwal188@gmail.com>
2023-01-29 20:05:26 +0530

    updated main.py

--- cool_concepts/encapsulation/main.py
+++ cool_concepts/encapsulation/main.py
@@ -1,22 +1,24 @@
 class Person:
-    def __init__(self, name, age) -> None:
+    def __init__(self, name, age):
+        # double underscore is used to make data members private
+        # now they can not be accessed outside the class directly
         self.__name = name 
         self.__age = age
     
-    @property 
+    @property  #getter
     def Name(self):
         return self.__name
     
-    @property
+    @property # getter
     def Age(self):
         return self.__age
     
-    @Age.setter
+    @Age.setter # setter
     def Age(self, value):
         if value>0:
             self.__age = value
             
-    def show(self):
+    def show(self): 
         print(self.__name, self.__age)
         
         
