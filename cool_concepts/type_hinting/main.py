13c558050a248049cc2011cd5698816f830a75cd
Divyansh Agarwal <divyanshagarwal188@gmail.com>
2023-01-30 15:57:09 +0530

    argument type and function return type

--- cool_concepts/type_hinting/main.py
+++ cool_concepts/type_hinting/main.py
@@ -1,8 +1,10 @@
 # since python is dynamically type there is no way to check or understand the types of arguemtns passed before run time
 # as the name suggests type hinting makes it easy for others to learn about the values of paramaters
 # this is done simply by using :
+# to highlight the function return type use ->
 
-def func(param: int):
+# function func takes an argument of type int and has a return value of none
+def func(param: int) -> None:
     print(param)
     
 func(1)
