822c660efe0489e0724a1596ef96f08e2a58705e
Divyansh Agarwal <divyanshagarwal188@gmail.com>
2023-02-08 22:28:53 +0530

    waiting for threadss

--- must_know_concepts/multithreading/main.py
+++ must_know_concepts/multithreading/main.py
@@ -12,5 +12,8 @@ t1 = threading.Thread(target=func1)
 t2 = threading.Thread(target=func2)
 
 # .start() is used to run threads parallely
-t1.start()
-t2.start()
+# t1.start()
+# t2.start()
+
+t1.start() 
+print("some code") # this statement does not wait for the above thread to finish
