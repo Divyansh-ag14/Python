383736cadfdb26b56f6eda3030d44f6baf215c4b
Divyansh Agarwal <divyanshagarwal188@gmail.com>
2023-02-10 23:38:30 +0530

    calling function of parent class in child class

--- must_know_concepts/super_keyword/main.py
+++ must_know_concepts/super_keyword/main.py
@@ -1,12 +1,15 @@
 class Parent:
-    def parent_method(self):
+    def func(self):
         print("I am parent class")
         
 class Child(Parent):
+    def func(self):
+        print("method of child class")
+        
     def child_method(self):
         print("I am child class!")
         
-        super().parent_method()
+        super().func()
     
 c1 = Child()
 c1.child_method()
